4c555f78e4bc46e1c61007ca6cb6f02c2865b6f6
Nicky <zenanicole1412@gmail.com>
2019-09-13 23:22:55 -0500

    1st version

---
+++ src/main/java/com/fairfield/event/eventfinder/EventfinderApplication.java
@@ -0,0 +1,13 @@
+package com.fairfield.event.eventfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fairfield/event/eventfinder/EventfinderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fairfield.event.eventfinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EventfinderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
